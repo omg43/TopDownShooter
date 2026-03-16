022eb2a37de194bcb505ef6d7ecc5c894c5672a9
Egor <e.yurchenko@xlab-game.com>
2026-03-16 19:25:04 +0300

    Try fix enemy

--- Assets/Scripts/Entities/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Entities/Enemies/EnemySpawner.cs
@@ -14,23 +14,18 @@ namespace Entities.Enemies
 
         private List<Enemy> m_currentEnemies = new();
 
-        public void DespawnAll()
-        {
-            foreach(var enemy in m_enemies)
-            {
-                DestroyEnemy(enemy);
-            }
-            m_currentEnemies.Clear();
-        }
-
         public void Spawn()
         {
+            var factory = ServiceLocator
+                .Resolve<IPlayerFactorySettings>();
+            factory.position = Vector3.negativeInfinity;
+
             var playerTransform = ServiceLocator
-                .Resolve<PlayerFactory>()
+                .Resolve<IPlayerFactory>()
                 .Create()
                 .transform;
 
-            foreach(var spawnPoint in m_spawnPoints)
+            foreach (var spawnPoint in m_spawnPoints)
             {
                 var enemy = GetEnemy();
                 var enemyData = GetEnemyData();
@@ -39,25 +34,39 @@ namespace Entities.Enemies
                 enemyInstance.Initialize(enemyData, playerTransform);
 
                 enemyInstance.Died += OnDied;
-                m_currentEnemies.Add(enemy);
+                m_currentEnemies.Add(enemyInstance);
             }
         }
 
         private void OnDied(Enemy enemy)
         {
+            m_currentEnemies.Remove(enemy);
             DestroyEnemy(enemy);
         }
 
         private Enemy GetEnemy() =>
             m_enemies[Random.Range(0, m_enemies.Length)];
-        
+
         private EnemyData GetEnemyData() =>
             m_data[Random.Range(0, m_data.Length)];
 
-        public void DestroyEnemy(Enemy enemy)
+        private void DestroyEnemy(Enemy enemy)
         {
-            enemy.Died -= OnDied;
-            Destroy(enemy.gameObject);
+            if (enemy)
+            {
+                enemy.Died -= OnDied;
+                Destroy(enemy.gameObject);
+            }
+        }
+
+        public void DespawnAll()
+        {
+            foreach (var enemy in m_currentEnemies)
+            {
+                DestroyEnemy(enemy);
+            }
+
+            m_currentEnemies.Clear();
         }
     }
 }
